418f1a4be7ea0b57042eb249d9d78872e8712537
William Brendaw <williambrendaw@icloud.com>
2026-05-29 17:21:07 -0300

    chore: update code formatting

--- src/extension.js
+++ src/extension.js
@@ -1,30 +1,27 @@
 /*
   Add Username to Top Panel GNOME extension
-  Created by William Brendaw (williambrendaw@protonmail.com)
+  Created by William Brendaw (william@brendaw.net)
 
   Inspired from Add User and Host name to panel (https://extensions.gnome.org/extension/813/add-user-and-host-name-to-panel/)
 
   Hack it!
 */
 
-import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
-import { UsernameIndicator } from './UsernameIndicator.js';
-import GObject from 'gi://GObject';
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+import { UsernameIndicator } from "./UsernameIndicator.js";
+import GObject from "gi://GObject";
 
 export default class UsernameIndicatorExtension extends Extension {
-    _indicator;
+  _indicator;
 
-    enable() {
-        this._indicator = new UsernameIndicator();
-    }
+  enable() {
+    this._indicator = new UsernameIndicator();
+  }
 
-    disable() {
-        this._indicator.destroy();
-        this._indicator = null;
-    }
+  disable() {
+    this._indicator.destroy();
+    this._indicator = null;
+  }
 }
 
-GObject.registerClass(
-    {GTypeName: 'UsernameIndicator'},
-    UsernameIndicator
-);
+GObject.registerClass({ GTypeName: "UsernameIndicator" }, UsernameIndicator);
