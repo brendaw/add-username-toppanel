ab1d76562adb4a38173d5d720cd0086a18b0e926
William Brendaw <williambrendaw@icloud.com>
2026-05-29 16:07:45 -0300

    chore: add update local install script

---
+++ update-local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source_folder=src
+
+extensions_folder="$HOME/.local/share/gnome-shell/extensions"
+extension_uuid="add-username-toppanel@brendaw.com"
+extension_folder="$extensions_folder/$extension_uuid"
+
+echo "$extension_folder"
+mkdir -p "$extension_folder"
+
+rm -rf "$extension_folder/*"
+
+cp -r src/* $extension_folder
+
+echo "Extension updated locally at $extension_folder"
