7cc742daf4d63a9755cfa1c7b8feb51472214926
William Brendaw <williambrendaw@icloud.com>
2026-08-18 16:15:52 -0300

    feat: add position control for username in panel (left, center, right)
    Closes #31

--- src/usernameIndicator.js
+++ src/usernameIndicator.js
@@ -19,18 +19,22 @@ export class UsernameIndicator extends SystemIndicator {
     this._applyStyle();
     this.add_child(this._usernameLabel);
 
+    const QuickSettingsMenu = Main.panel.statusArea.quickSettings;
+    this._indicatorsBox = QuickSettingsMenu._indicators;
+
+    this._childAddedId = this._indicatorsBox.connect("child-added", () =>
+      this._applyPosition(),
+    );
+
     this._signalIds = [
       settings.connect("changed::display-text", () => this._updateText()),
       settings.connect("changed::padding", () => this._applyStyle()),
       settings.connect("changed::spacing", () => this._applyStyle()),
+      settings.connect("changed::position", () => this._applyPosition()),
     ];
 
-    const QuickSettingsMenu = Main.panel.statusArea.quickSettings;
-    this._indicatorsBox = QuickSettingsMenu._indicators;
-    this._childAddedId = this._indicatorsBox.connect("child-added", () =>
-      this._indicatorsBox.set_child_above_sibling(this, null),
-    );
     QuickSettingsMenu.addExternalIndicator(this);
+    this._applyPosition();
   }
 
   _getUsername() {
@@ -62,6 +66,24 @@ export class UsernameIndicator extends SystemIndicator {
     this._usernameLabel.set_style(
       `padding-left: ${padding}px; padding-right: ${padding}px; margin-left: ${spacing}px; margin-right: ${spacing}px;`,
     );
+  }
+
+  _applyPosition() {
+    const position = this._settings.get_string("position");
+    const box = this._indicatorsBox;
+
+    if (position === "left") {
+      const firstChild = box.get_first_child();
+      if (firstChild && firstChild !== this) {
+        box.set_child_below_sibling(this, firstChild);
+      }
+    } else if (position === "center") {
+      const count = box.get_n_children();
+      const middle = Math.floor(count / 2);
+      box.set_child_at_index(this, middle);
+    } else {
+      box.set_child_above_sibling(this, null);
+    }
   }
 
   destroy() {
